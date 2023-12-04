83b5ac85f83b4e7b630974bc4f06eeb84f8177d5
aautin <ezagof@gmail.com>
2023-12-04 12:13:38 +0100

    add the local libft (new structure of linked list)

---
+++ libft/src/ft_lstclear.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 13:26:06 by aautin            #+#    #+#             */
+/*   Updated: 2023/12/02 13:02:16 by aautin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+void	ft_lstclear(t_list **lst)
+{
+	t_list	*tmp;
+	t_list	*start;
+
+	if (!lst)
+		return ;
+	start = *lst;
+	while (start)
+	{
+		tmp = start->next;
+		free(start);
+		start = tmp;
+	}
+	*lst = NULL;
+}
